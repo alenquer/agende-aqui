5b38e1ff8f92ad8d958cbcccca48f56b5b1ece40
alenquer <dev.alenquer@gmail.com>
2023-08-13 11:29:25 -0300

    create chip component

---
+++ app/_components/chip/index.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { twMerge } from "tailwind-merge";
+
+export interface IChipProps extends React.HTMLAttributes<HTMLButtonElement> {
+	label: string;
+	isActive?: boolean;
+}
+
+export const Chip: React.FC<IChipProps> = ({ label, isActive, className, ...rest }) => {
+	return (
+		<button
+			className={twMerge(
+				"px-4",
+				"py-2",
+				"rounded-md",
+				"hover:opacity-50",
+				isActive ? "bg-primary" : "bg-slate-200",
+				className
+			)}
+			{...rest}
+		>
+			<span className={twMerge(isActive ? "text-white" : "text-slate-400", "text-sm")}>{label}</span>
+		</button>
+	);
+};
