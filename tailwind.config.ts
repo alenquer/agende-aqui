9f3aa1c9dd920c0093115dc111465049860db356
alenquer <dev.alenquer@gmail.com>
2023-08-13 11:16:45 -0300

    update app layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
+	theme: {
+		extend: {
+			width: {
+				"page-content": "min(1024px, 90%)"
+			},
+			screens: {
+				mb: "520px"
+			},
+			colors: {
+				primary: "#6E66DD",
+				secondary: "#3B30B2"
+			}
+		}
+	},
+	plugins: []
+};
+
+export default config;
